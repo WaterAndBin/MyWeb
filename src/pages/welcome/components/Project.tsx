9f76b4cffda1bd8ac6314b25294fd47ee5a8b3c3
WaterAndBin <1336263157@qq.com>
2024-05-22 19:39:13 +0800

    5/22

--- src/pages/welcome/components/Project.tsx
+++ src/pages/welcome/components/Project.tsx
@@ -9,11 +9,17 @@ export default function Project(): ReactElement {
   });
 
   return (
-    <div className="h-full w-full project-shadow flex justify-center">
-      <div className="h-full">
-        <animated.div style={{ ...springs }} className="bg-black w-2">
+    <div className="h-full w-full bg-[#212121]">
+      <div className="h-full box-border grid grid-cols-2 gap-4 items-center justify-center">
+        {/* <animated.div style={{ ...springs }} className="bg-black w-2">
           <div></div>
-        </animated.div>
+        </animated.div> */}
+        <div className="w-full h-full">
+          <div className="projectBox ">123123</div>
+        </div>
+        <div className="projectBox">123123</div>
+        <div className="projectBox">123123</div>
+        <div className="projectBox">123123</div>
       </div>
     </div>
   );
